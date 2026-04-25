cb54ae2fab950358a21f8951c227c5d9116df7b6
amnasrya <151316250+vischachawww@users.noreply.github.com>
2026-04-25 18:29:36 +0800

    security

--- BackendTemplate/src/main/java/Hackathing/BackendTemplate/Security/SecurityConfig.java
+++ BackendTemplate/src/main/java/Hackathing/BackendTemplate/Security/SecurityConfig.java
@@ -24,7 +24,7 @@ public class SecurityConfig {
                 .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/**").permitAll()
-                        .requestMatchers("/inventory/**").authenticated()
+                        .requestMatchers("/inventory/**").permitAll()
                         .requestMatchers("/test/**").permitAll()
                         .anyRequest().permitAll()
                 )
